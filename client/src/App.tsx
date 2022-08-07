c9996f8c3236e3ed6d2b82de3fa491cea0b456ff
Jinny <khj9709@icloud.com>
2022-08-07 23:30:05 +0900

    feat: :sparkles: 간단한 mixin 추가 #10

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,32 @@
 import styled from 'styled-components';
+import mixin from './cores/styles/mixin';
 
 function App() {
-  const a = [1, 23, 4, 5];
   return (
     <div>
-      {a.map((elem) => (
-        <li key={elem}>{elem}</li>
-      ))}
-
-      <P>테스트</P>
-      <P>테스트</P>
-      <P>테스트</P>
-      <P>테스트</P>
+      <P>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint culpa, perspiciatis cum sequi
+        quae dolores distinctio hic eius iusto commodi neque odit voluptatem. Quibusdam in, voluptas
+        aliquam adipisci incidunt quidem.
+      </P>
+      <PPAP />
     </div>
   );
 }
 
 const P = styled.p`
+  width: 200px;
   color: ${({ theme }) => theme.colors.primary};
+  ${mixin.textEllipsis(2)}
+`;
+
+const PPAP = styled.p`
+  width: 200px;
+  height: 300px;
+  ${mixin.backgroundImage(
+    'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1567008394/noticon/ohybolu4ensol1gzqas1.png',
+    { contain: true, repeat: true },
+  )}
 `;
 
 export default App;
