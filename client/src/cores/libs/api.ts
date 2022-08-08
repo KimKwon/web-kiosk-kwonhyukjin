b4736cca7c8622086fc852c1d16a3d673b60df54
Jinny <khj9709@icloud.com>
2022-08-08 17:34:14 +0900

    feat: :sparkles: proxy를 통해 cors 우회 #12

--- client/src/cores/libs/api.ts
+++ client/src/cores/libs/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const remote = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api' : '/api',
+  baseURL: '/api',
 });
