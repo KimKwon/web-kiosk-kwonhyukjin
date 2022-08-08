4081260d749428be4b51c4eb7ada9e3e58befc12
Jinny <khj9709@icloud.com>
2022-08-08 20:47:04 +0900

    feat: :sparkles: useAPI lazy모드 추가 #14
    before) 기존 API 훅은 effect에 의해 무조건 마운트 시 호출되는 구조
    after) isLazy 파라미터에 의해 사용자가 원하는 타이밍에 호출가능하도록 변경
    
    point) BaseAPI와 LazyAPI 타입으로 단언 필요, 제네릭 제공 필수

--- client/src/cores/hooks/useAPI.tsx
+++ client/src/cores/hooks/useAPI.tsx
@@ -14,21 +14,36 @@ interface APIError {
   code?: number;
 }
 
-function useAPI<ResponseData, RequestData = Record<string, never>>(config: APIConfig<RequestData>) {
-  const [response, setResponse] = useState<ResponseData>();
+export interface BaseAPI<Response> {
+  data?: Response;
+  isLoading: boolean;
+  refetch: () => void;
+  error?: APIError;
+}
+
+export type LazyAPI<Response> = [
+  () => Promise<Response | undefined>,
+  Pick<BaseAPI<Response>, 'isLoading' | 'error'>,
+];
+
+function useAPI<RequestData = Record<string, never>>(
+  config: APIConfig<RequestData>,
+  isLazy = false,
+) {
+  const [response, setResponse] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<APIError>();
   const [shouldRefetch, setShouldRefetch] = useState(0);
 
   const refetch = useCallback(() => setShouldRefetch((updateCount) => updateCount + 1), []);
 
-  useEffect(() => {
+  const fetchFn = async () => {
     try {
-      (async function () {
-        setIsLoading(true);
-        const result = await remote.request(config);
-        setResponse(result?.data);
-      })();
+      setIsLoading(true);
+      const result = await remote.request(config);
+      setResponse(result?.data);
+
+      return result?.data;
     } catch (error) {
       /**
        * TODO :: 선언적 에러핸들링 설계해보기
@@ -38,6 +53,7 @@ function useAPI<ResponseData, RequestData = Record<string, never>>(config: APICo
        */
 
       if (axios.isAxiosError(error)) {
+        console.log('isAxiosError');
         /**
          * TODO :: 에러코드 정의하기
          *
@@ -58,8 +74,15 @@ function useAPI<ResponseData, RequestData = Record<string, never>>(config: APICo
     } finally {
       setIsLoading(false);
     }
+  };
+
+  useEffect(() => {
+    if (isLazy) return;
+    fetchFn();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [shouldRefetch]);
+
+  if (isLazy) return [fetchFn, { isLoading, error }];
 
   return { data: response, isLoading, error, refetch };
 }
