ee59f989287b6e5cab88268cb0498806534c04b6
Jinny <khj9709@icloud.com>
2022-08-10 15:19:09 +0900

    feat: :sparkles: 각 결제수단의 핸들러 스켈레톤 컴포넌트 작성 #23

---
+++ client/src/components/PaymentManager/CardPaymentHandler.tsx
@@ -0,0 +1,5 @@
+function CardPaymentHandler() {
+  return <div>카드결제담당자</div>;
+}
+
+export default CardPaymentHandler;
---
+++ client/src/components/PaymentManager/CashPaymentHandler.tsx
@@ -0,0 +1,5 @@
+function CashPaymentHandler() {
+  return <div>현금결제담당자</div>;
+}
+
+export default CashPaymentHandler;
