f13e843a8e24d2337c4772044454bb2b488da0e7
Jinny <khj9709@icloud.com>
2022-08-12 03:14:04 +0900

    feat: :sparkles: 에러 타입체크 함수 작성 #35

---
+++ client/src/utils/error.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import ERROR_MSG from '../constants/error-message';
+
+export const isKnownError = (
+  error: unknown,
+): error is {
+  response: {
+    status: keyof typeof ERROR_MSG;
+  };
+} => {
+  if (!(error instanceof Error)) return false;
+  if (!axios.isAxiosError(error)) return false;
+  if (!error.response) return false;
+
+  const STATUS_CODE = error.response.status;
+
+  console.log(STATUS_CODE);
+
+  if (STATUS_CODE in ERROR_MSG) {
+    return true;
+  }
+
+  return false;
+};
