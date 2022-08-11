98cc4b098ec748de534bf840e24030f4265bc157
Jin <khj9709@icloud.com>
2022-08-11 14:31:58 +0900

    refac: :hammer: 타이머 기능 useTimer 훅으로 분리 #28

---
+++ client/src/cores/hooks/useTimer.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useTimer(initialTimeout: number) {
+  const [remainTime, setRemainTime] = useState(initialTimeout);
+  const remainTimeRef = useRef<number>(initialTimeout);
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
+
+  useEffect(() => {
+    remainTimeRef.current = remainTime;
+  }, [remainTime]);
+
+  const _setInterval = (timeout: number, cb: () => unknown) => {
+    setRemainTime(timeout);
+    intervalRef.current = setInterval(() => {
+      setRemainTime((prevRemainTime) => prevRemainTime - 1);
+
+      if (remainTimeRef.current - 1 === 0) {
+        _clearInterval();
+        cb();
+      }
+    }, 1000);
+  };
+
+  const _clearInterval = () => {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  };
+
+  const invoke = (cb: () => unknown) => {
+    if (intervalRef.current) return;
+
+    _setInterval(remainTime, cb);
+  };
+
+  const pause = () => {
+    _clearInterval();
+  };
+
+  const clear = () => {
+    _clearInterval();
+    setRemainTime(initialTimeout);
+  };
+
+  return { remainTime, invoke, clear, pause };
+}
+
+export default useTimer;
