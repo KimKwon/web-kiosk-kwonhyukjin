c02ee2a2307f0e2e5f848ffde22ab588fc9c1472
Jinny <khj9709@icloud.com>
2022-08-12 03:59:57 +0900

    feat: :sparkles: 인트로페이지 간단하게 구현 #13

--- client/src/cores/styles/global-style.ts
+++ client/src/cores/styles/global-style.ts
@@ -11,6 +11,14 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 
+  @font-face {
+    font-family: 'BMDOHYEON';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMDOHYEON.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   body {
     font-family: Pretendard Variable, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
