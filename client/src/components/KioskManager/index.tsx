88817f7092e69b6b5545e41a79654b01d1190b74
Jin <khj9709@icloud.com>
2022-08-11 11:50:15 +0900

    feat: :sparkles: 메뉴가 이미 있으면 수량/합계 증가 #33

--- client/src/components/KioskManager/index.tsx
+++ client/src/components/KioskManager/index.tsx
@@ -64,7 +64,43 @@ function KioskManager({ data }: KioskProps) {
     setCartInfoList([]);
   };
 
-  const addCartInfo = (newCartInfo: Omit<CartInfoType, 'cartElementId'>) => {
+  const findCartInfoId = (targetCartInfo: CartInfoCreateDto) => {
+    const { menuId: targetMenuId, sizeId: targetSizeId, isIce: targetIsIce } = targetCartInfo;
+    return cartInfoList.find(
+      ({ menuId, sizeId, isIce }) =>
+        targetMenuId === menuId && targetSizeId === sizeId && targetIsIce === isIce,
+    )?.cartElementId;
+  };
+
+  const updateCartInfo = (
+    targetCartInfoId: number,
+    updateInfo: Pick<CartInfoCreateDto, 'total' | 'amount'>,
+  ) => {
+    const copiedCartInfoList = [...cartInfoList];
+    const targetCartInfo = copiedCartInfoList.find(
+      ({ cartElementId }) => targetCartInfoId === cartElementId,
+    );
+
+    if (!targetCartInfo) return;
+
+    const { total, amount } = updateInfo;
+
+    targetCartInfo.amount += amount;
+    targetCartInfo.total += total;
+
+    setCartInfoList(copiedCartInfoList);
+  };
+
+  const addCartInfo = (newCartInfo: CartInfoCreateDto) => {
+    const existCartInfoId = findCartInfoId(newCartInfo);
+    const isCartInfoAlreadyExist = existCartInfoId !== undefined;
+
+    if (isCartInfoAlreadyExist) {
+      const { amount, total } = newCartInfo;
+      updateCartInfo(existCartInfoId, { amount, total });
+      return;
+    }
+
     setCartInfoList((prevCartInfoList) => [
       ...prevCartInfoList,
       {
