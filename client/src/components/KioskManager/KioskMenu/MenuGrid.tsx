b384a74ea7900bfa230b22d591225f040e17703b
Jinny <khj9709@icloud.com>
2022-08-11 22:02:17 +0900

    feat: :sparkles: 메뉴 목록 슬라이드 애니메이션 추가 #37

--- client/src/components/KioskManager/KioskMenu/MenuGrid.tsx
+++ client/src/components/KioskManager/KioskMenu/MenuGrid.tsx
@@ -2,26 +2,40 @@ import styled from 'styled-components';
 
 interface MenuGridProps {
   children: JSX.Element[];
+  selectedCategoryIdx: number;
 }
 
 function MenuGrid(props: MenuGridProps) {
-  const { children } = props;
-  return <SectionContainer>{children}</SectionContainer>;
+  const { children, selectedCategoryIdx } = props;
+
+  const currentTranslateX = `translateX(${-100 * selectedCategoryIdx}%)`;
+
+  return <GridSection currentTranslateX={currentTranslateX}>{children}</GridSection>;
 }
 
-const SectionContainer = styled.section`
+const GridSection = styled.section<{ currentTranslateX: string }>`
   flex: 2;
 
   height: 600px;
-  overflow-y: auto;
-  padding: 5px;
 
-  display: grid;
-  grid-template-columns: repeat(3, 198px);
-  justify-content: flex-start;
-  gap: 24px;
+  display: flex;
+  overflow-x: hidden;
 
-  user-select: none;
+  & > div.translated-box {
+    transform: ${({ currentTranslateX }) => currentTranslateX};
+    transition: transform ease 0.3s;
+    flex: 0 0 auto;
+    width: 100%;
+    overflow-y: auto;
+    padding: 5px;
+
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-content: flex-start;
+    gap: 24px;
+
+    user-select: none;
+  }
 `;
 
 export default MenuGrid;
